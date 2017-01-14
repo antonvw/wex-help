c908c845386f5a9c0c09857b04707d525d340572
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2017-01-14 20:57:40 +0100

    add files

---
+++ regen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# regenerate doxy pages for next wxExtension version
+
+# location where wxExtension repository resides
+export WXEXTENSION=${HOME}/wxExtension/extension
+
+current=$(pwd)
+
+# version directory where pages are generated
+cd v17.04.0
+
+# now run doxygen
+doxygen ../Doxyfile
+
+cd $current
